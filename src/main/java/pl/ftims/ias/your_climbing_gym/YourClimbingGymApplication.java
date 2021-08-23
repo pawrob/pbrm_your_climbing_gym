dc5fffff4f02c8b38cbbf34ca9bd05202c348bf7
224270 Bucki Paweł <224270@edu.p.lodz.pl>
2021-08-23 16:47:55 +0200

    init commit

---
+++ src/main/java/pl/ftims/ias/your_climbing_gym/YourClimbingGymApplication.java
@@ -0,0 +1,13 @@
+package pl.ftims.ias.your_climbing_gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourClimbingGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourClimbingGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ftims/ias/your_climbing_gym/YourClimbingGymApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ftims.ias.your_climbing_gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourClimbingGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
